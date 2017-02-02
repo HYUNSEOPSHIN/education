9ad08e137e067e717489068f6433ed42be24ff04
acorn <acorn@acorn-PC>
2017-02-02 11:43:35 +0900

    Third Commit Work_eclipse

--- HelloGit/src/test/Hellojava.java
+++ HelloGit/src/test/Hellojava.java
@@ -5,10 +5,13 @@ public class Hellojava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java~!");
-		System.out.println("First Commit work!");
+		System.out.println("First Commit work~!");
 		
 		//branch 내용 추가 - 인코딩 
-		System.out.println("Second Commit work!");
+		System.out.println("Second Commit work~~!");
+		
+		//지역저장소 내용 추가
+		System.out.println("Third Commit work~~~!");
 
 	}
 
