d2b6978144cf5236ee106f29025faea76a331dee
acorn <acorn@acorn-PC>
2017-02-02 11:07:14 +0900

    Second Commit work!

--- HelloGit/src/test/Hellojava.java
+++ HelloGit/src/test/Hellojava.java
@@ -6,6 +6,9 @@ public class Hellojava {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java~!");
 		System.out.println("First Commit work!");
+		
+		//branchÇÑ ³»¿ë
+		System.out.println("Second Commit work!");
 
 	}
 
